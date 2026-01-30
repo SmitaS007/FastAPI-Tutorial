c47bfb89cca00d3b93c853ac73301797d9ea3a30
SmitaS007 <smitadshinde007@gmail.com>
2026-01-30 19:32:51 +0530

    Add Field_validator file

--- FastAPI Tutorial/PydanticTutorial/pydantic02.py
+++ FastAPI Tutorial/PydanticTutorial/pydantic02.py
@@ -1,11 +1,15 @@
-from pydantic import BaseModel
-from typing import List, Dict, Optional
+from pydantic import BaseModel, EmailStr, AnyUrl, Field
+from typing import List, Dict, Optional, Annotated
 
 class Patient(BaseModel):
 
-    name: str
-    age: int
-    weight: float
+    name: Annotated[str, Field(max_length= 50, title= "Name of the patient",
+                               description='Give the name of a patient in less that 50 ch',
+                               examples=['Smita', 'Snehal'])]
+    #email: EmailStr
+    #linkedin_url: AnyUrl
+    age: int = Field(gt=0, lt=120)
+    weight: float = Field(gt=0)
     married: bool = False
     allergies: Optional[List[str]]=None
     contact_details: Dict[str, str]
